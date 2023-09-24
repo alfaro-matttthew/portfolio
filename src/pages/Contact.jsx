1a82df25781afd6efa8fca38c937558b4006f07b
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-09-24 17:08:49 -0400

    retry

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -86,11 +86,14 @@ export default function Contact() {
       </div>
       <div className='row'>
         <div className="min-vh-100 w-50 container">
-        <form name='contact' netlify netlify-honeybot='bot-field' hidden>
-          <input type='text' name='name' />
-          <input type='email' name='email' />
-          <textarea name='message'></textarea>
-        </form>
+
+            <form style={formStyles} name='contact' action='/contacts' method='post'>
+                <input type='hidden' name='form-name' value='contact' />
+                <input required type='text' name='name' placeholder='Your Name' />
+                <input required type='email' name='email' placeholder='Your Email' />
+                <textarea required  name='message' placeholder='message' cols='30' rows='10' ></textarea>
+                <button type='submit'>Send a Message</button>
+            </form>
 
           {/* {isFormSubmitted ? (
             <FormSuccess name={formData.firstName} onReset={handleReset} />
