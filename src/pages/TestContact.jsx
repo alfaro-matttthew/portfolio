451468008620a67d072489b1c57e4a37cf808984
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-09-24 22:33:52 -0400

    new test contact

--- src/pages/TestContact.jsx
+++ src/pages/TestContact.jsx
@@ -10,13 +10,42 @@ export default function TestContact() {
 
     return (
         <div>
-            {/* <form style={formStyles} name='contact' action='/contacts' method='post'>
-                <input type='hidden' name='form-name' value='contact' />
-                <input required type='text' name='name' placeholder='Your Name' />
-                <input required type='email' name='email' placeholder='Your Email' />
-                <textarea required  name='message' placeholder='message' cols='30' rows='10' ></textarea>
+            <form 
+                style={formStyles} 
+                name='contact v1' 
+                action='/contacts' 
+                method='post'
+                data-netlify='true'
+                onSubmit='submit'
+            >
+               <div>
+                    <label>First Name <br/>
+                        <input type='text' name='first-name'/>
+                    </label>
+               </div>
+
+                <div>
+                    <label htmlFor='email'>Email</label> <br/>
+                    <input id='email' type='email' name='email' />
+                </div>
+
+                <div>
+                    <label>Anyy Comments?
+                        <textarea name='comments'></textarea>
+                    </label>
+                </div>
+
                 <button type='submit'>Send a Message</button>
-            </form> */}
+            </form>
         </div>
+        // <div>
+        //     <form style={formStyles} name='contact' action='/contacts' method='post'>
+        //         <input type='hidden' name='form-name' value='contact' />
+        //         <input required type='text' name='name' placeholder='Your Name' />
+        //         <input required type='email' name='email' placeholder='Your Email' />
+        //         <textarea required  name='message' placeholder='message' cols='30' rows='10' ></textarea>
+        //         <button type='submit'>Send a Message</button>
+        //     </form>
+        // </div>
     )
 }
