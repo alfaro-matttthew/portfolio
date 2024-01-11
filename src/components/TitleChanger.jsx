179e42120313117c6e3cc8a47f3a87ef95dbc8c4
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2024-01-11 18:35:11 -0500

    fixed bugs with intervals and invoking functions

--- src/components/TitleChanger.jsx
+++ src/components/TitleChanger.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 
-const TitleChanger = () => {
+export default function TitleChanger() {
     const skillNames = ['Graphic Designer', 'Front-End Developer'];
     // const text = document.querySelector('.text');
 
@@ -17,25 +17,28 @@ const TitleChanger = () => {
 
     const textRef = React.useRef(null);
 
-    const writeSkillName = (skill) => {
+    const writeSkillNames = (skill) => {
         let i = 0;
         textRef.current.innerHTML = "";
     
         let id = setInterval(() => {
             if (i >= skill.length) {
-                deleteSkillName();
+                let pause = setInterval(() => {
+                    deleteSkillName();
+                    clearInterval(pause);
+                }, 3000);
                 clearInterval(id);
             } else {
                 textRef.current.innerHTML += skill[i];
                 i++;
             }
-        }, 250);
+        }, 100);
     };
 
     const deleteSkillName = () => {
         let skill = textRef.current.innerHTML;
-        let i = word.length - 1;
-
+        let i = skill.length - 1;  // Fix: use skill.length instead of word.length
+    
         let id = setInterval(() => {
             if (i >= 0) {
                 textRef.current.innerHTML = textRef.current.innerHTML.substring(0, textRef.current.innerHTML.length - 1);
@@ -44,15 +47,19 @@ const TitleChanger = () => {
                 writeSkillNames(skillNames[gen.next().value]);
                 clearInterval(id);
             }
-        }, 100);
+        }, 40);
     };
 
+    const gen = generator();
+
     useEffect(() => {
-        const gen = generator();
         writeSkillNames(skillNames[gen.next().value]);
     }, []);
 
     return (
+        // <h1 ref={textRef} className="text"></h1>
+
         <h1 ref={textRef} className="text stroke font-san-serif title lh-1 text-uppercase"></h1>
     )
 };
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className=" col-12 col-lg-5 home-start d-flex align-items-center">
         <div className=" w-100 px-3 d-flex flex-column align-items-start justify-content-center" style={ color }>
           <span className="home-back font-serif fs-3 lh-1">creative</span>
-            <TitlteChanger />
+            <TitleChanger />
           <span className="home-back font-serif fs-3 lh-1">based in tampa, florida</span>
           <ButtonPillNav title={"Let's Chat"} path={'/Contact'} style={{color: '#28231D'}}/>
         </div>
